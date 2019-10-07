5180be173d2535192b44b357bb1374717ceb109b
DESKTOP-76705DU\mihai <danm@oregonstate.edu>
2019-10-07 11:54:41 -0700

    github pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
               <Navigation>
                   <Link to="/">Home</Link>
                   <Link to="/resume">Resume</Link>
-                  <Link to="/aboutme">About Me</Link>
+                  {/*<Link to="/aboutme">About Me</Link>*/}
                   <Link to="/projects">Projects</Link>
                   <Link to="/contact">Contact</Link>
               </Navigation>
@@ -21,7 +21,7 @@ function App() {
               <Navigation>
                   <Link to="/">Home</Link>
                   <Link to="/resume">Resume</Link>
-                  <Link to="/aboutme">About Me</Link>
+                  {/*<Link to="/aboutme">About Me</Link>*/}
                   <Link to="/projects">Projects</Link>
                   <Link to="/contact">Contact</Link>
               </Navigation>
